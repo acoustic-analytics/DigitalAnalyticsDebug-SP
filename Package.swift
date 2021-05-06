e2e8dacc208efa4f8915bca749cb1a406cfc8734
Omar Hernandez <omar.hernandez@acoustic.co>
2021-05-06 15:57:56 -0700

    EAS-289: Added Swift Package for DA.

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Copyright (C) 2021 Acoustic, L.P. All rights reserved.
+//
+// NOTICE: This file contains material that is confidential and proprietary to
+// Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+// industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+// Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+// prohibited.
+
+import PackageDescription
+
+print("Using DAMod Debug version, if you need release version use https://github.com/acoustic-analytics/DigitalAnalytics-SP")
+let package = Package(
+    name: "DAMod",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "DAMod",
+            targets: ["DAMod", "EOCore"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        //
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "DAMod",
+            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.38/DAMod_XCFramework_Debug.zip",
+            checksum: "288da876235c22e3cca01e18557e59b08d6cbeaa376dc9a05f5b8364034cb5ae"),
+        .binaryTarget(
+            name: "EOCore",
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.33/EOCore_XCFrameworks_Debug.zip",
+            checksum: "4b0a2a39c73cd8512b3e6e876609f9fb50e1a718c9f9e42503d2713e81a794b8"),
+    ]
+)
