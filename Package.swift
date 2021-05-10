da3b14830593b42a342684d51456adc8c9da09d4
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-05-10 13:33:23 -0700

    Beta DAMod build: 1_1_43
    Beta DAMod Change Notes:
    Enhancements
    
    - None.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - arm64e bitcode slice is not created. Filed support ticket with Apple.

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DAMod",
-            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.42/DAMod_XCFramework_Debug.zip",
-            checksum: "08b741e420345245627b40788afd77b89af6a21f2275bebe712f2c84e8866af1"),
+            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.43/DAMod_XCFramework_Debug.zip",
+            checksum: "e996ba4284bacfeb9bf4c36e1b630953f00cd3d4081716ebe85402804ab54b1b"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.48/EOCore_XCFramework_Debug.zip",
