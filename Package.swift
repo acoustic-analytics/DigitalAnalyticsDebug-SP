feb50af59adb13cf685939604c4917e915f18ab4
Omar Hernandez <omar.hernandez@acoustic.co>
2021-05-26 13:53:38 -0700

    Beta DAMod build: 1_1_44
    Beta DAMod Change Notes:
    Enhancements
    
    - Library is now being built for xcframeworks with latest xcode 12.5.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DAMod",
-            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.43/DAMod_XCFramework_Debug.zip",
-            checksum: "e996ba4284bacfeb9bf4c36e1b630953f00cd3d4081716ebe85402804ab54b1b"),
+            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.44/DAMod_XCFramework_Debug.zip",
+            checksum: "2c7563478f0089c24a4a3f06e9b683bb75534b30056593ab4eb7071c8ceaed91"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.49/EOCore_XCFramework_Debug.zip",
