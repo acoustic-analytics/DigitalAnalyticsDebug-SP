b0646a0b8ad34c4bb9dad0b0674057bccef97961
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-09-13 17:51:45 -0700

    Beta EOCore build: 2_3_59
    Beta EOCore Change Notes:
    Enhancements
    
    - Library is now being built for xcframeworks with latest xcode 12.5.
    
    Bug Fixes
    
    - Added extra checks and synchronized for queue that is posting back to server during application going to background or to terminate. There was some crashes reported, but unable to reproduce locally.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
             checksum: "3a7da74ff2b8a65096a1dd2eab85e84e27874e9d557baa9cfd1387c0a32d6306"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.58/EOCore_XCFramework_Debug.zip",
-            checksum: "8f5cb10008ae9dcf22886843d12dbae4be224cf858e48fbd8c1de163b2b0a5c6"),
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.59/EOCore_XCFramework_Debug.zip",
+            checksum: "aec6f059d24780f28f2927901dc0d58bdf0a71973090d2848a4025df5f29d128"),
     ]
 )
