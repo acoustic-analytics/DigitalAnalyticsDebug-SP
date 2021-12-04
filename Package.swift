29deae219a21e8d31f5964fbc79339c0f3655c75
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-12-04 10:12:57 -0800

    Beta EOCore build: 2.3.128
    Beta EOCore Change Notes:
    Enhancements
    
    - None.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "b7e78ec3fa75aae54e6e448447e93092ebd76e8c350d005b80456405cac2c3e7"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.127/EOCore_XCFramework_Debug.zip",
-            checksum: "36633c6612a2f2a4e9c7005d1bd5dad2b7dfe2282a2128474d0d91f337bab293"),
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.128/EOCore_XCFramework_Debug.zip",
+            checksum: "ae1c69f5f3dbb3947bad435e78e15ac5fbee460ec9dd1b8acca1f00bbd546d39"),
     ]
 )
