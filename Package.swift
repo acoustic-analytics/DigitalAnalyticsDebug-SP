732be51d0d2f0b3e23ed601e691fa34d52e57424
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-06-09 15:58:19 -0700

    Beta DAMod build: 1.1.78
    Beta DAMod Change Notes:
    Bug Fixes
    
    - None.
    
    Improvements:
    
    - New minimal os is 12.0 from 9.0 for library.
    
    Our environment for this release:
    
    - Xcode 13.2.1
    - MacOS 11.6.1
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - None.

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DAMod",
-            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.77/DAMod_XCFramework_Debug.zip",
-            checksum: "33b6a9ea6354e57d4bfd0f0d38d9280dca2cc792a36c240b68776e02f0eeb934"),
+            url: "https://github.com/acoustic-analytics/DigitalAnalytics/releases/download/1.1.78/DAMod_XCFramework_Debug.zip",
+            checksum: "2d4c3b5953545f186f3cf124930835f604d52f3623aa907359dd832df303f442"),
         .binaryTarget(
             name: "EOCore",
             url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.222/EOCore_XCFramework_Debug.zip",
