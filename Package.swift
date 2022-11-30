cc526929437f5a569e8208af71000479838164ec
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-11-30 13:51:17 -0800

    Beta EOCore build: 2.3.257
    Beta EOCore Change Notes:
    Bug Fixes
    
    - None.
    
    Improvements:
    
    - Add ability to post data during a fatalError in Swift via synchronous post.
    
    Our environment for this release:
    
    - Xcode 14.1
    - MacOS 13.0.1
    - iOS 12.x to 16.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
             checksum: "89d0970c48d3ad4cdb9f8e5fd51a90055e2e0b6d71ceb37807dc60ae7a252021"),
         .binaryTarget(
             name: "EOCore",
-            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.256/EOCore_XCFramework_Debug.zip",
-            checksum: "e23124b142e51bdbd9f26c7136f3cfdce41246418187bc9652cf8877f36ff8a9"),
+            url: "https://github.com/acoustic-analytics/EOCore/releases/download/2.3.257/EOCore_XCFramework_Debug.zip",
+            checksum: "092785e2556284313371f169f7be125f2f0b46f28fa5634c6ff9fde3ed3704d0"),
     ]
 )
